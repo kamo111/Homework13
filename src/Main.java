499b433b9ef4faa3bfb75e97c6324b6ef6a37def
kamo111 <kamo19.989king@gmail.com>
2024-06-13 04:11:40 +0300

    HW-13 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+                System.out.println("Задание1.");
+                Author ivan = new Author("Иван", "Иванович");
+                Author andrey = new Author("Андрей", "Андреевич");
+
+                Book book1 = new Book("Война и мир", ivan, 1998);
+                System.out.println("Название = " + book1.getName());
+                System.out.println("Автор = " + book1.getAuthor());
+                System.out.println("Год издания = " + book1.getYear());
+                book1.setYear(1888);
+                System.out.println("Новый год издания = " + book1.getYear());
+                System.out.println("-----------------");
+
+                Book book2 = new Book("Унесенные ветром", andrey, 1655);
+                System.out.println("Название = " + book2.getName());
+                System.out.println("Автор = " + book2.getAuthor());
+                System.out.println("Год издания = " + book2.getYear());
+                System.out.println(book1.equals(book2));
+                System.lineSeparator();
+            }
+        }
